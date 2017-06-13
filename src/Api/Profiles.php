74b9686ec1345d2dccafd7c3b9055613bd857666
Rafael Villa-Verde <villa655321verde@gmail.com>
2017-06-13 15:28:36 -0300

    profile and rankings api

--- src/Api/Profiles.php
+++ src/Api/Profiles.php
@@ -20,7 +20,159 @@
 namespace Hafael\Easyrec\Api;
 
 
-class Profile
+class Profiles extends Api
 {
+    /**
+     * This method stores the given profile to the item defined by the tenantid,itemid and the itemtypeid.
+     * If there is already a profile it will be overwritten..
+     *
+     * @param $itemId
+     * @param $itemType
+     * @param $profile
+     * @return array|mixed
+     */
+    public function store($itemId, $itemType, $profile)
+    {
+        return $this->_post("profile/store", [
+            "itemid" => $itemId,
+            "itemtype" => $itemType,
+            "profile" => $profile,
+        ]);
+    }
+
+    /**
+     * This method stores the given profile to the item defined by the tenantid,itemid and the itemtypeid.
+     * If there is already a profile it will be overwritten..
+     *
+     * @param $itemId
+     * @param $itemDescription
+     * @param $itemUrl
+     * @param null $itemImageUrl
+     * @param $itemType
+     * @param $profile
+     * @return array|mixed
+     */
+    public function storeItemWithProfile($itemId, $itemDescription, $itemUrl, $itemImageUrl = null, $itemType = null, $profile)
+    {
+        return $this->_post("profile/storeitemwithprofile", [
+            "itemid" => $itemId,
+            "itemdescription" => $itemDescription,
+            "itemurl" => $itemUrl,
+            "itemimageurl" => $itemImageUrl,
+            "itemtype" => $itemType,
+            "profile" => $profile,
+        ]);
+    }
+
+    /**
+     * This method deletes the profile of the item defined by the tenantid, itemid and the itemtypeid.
+     *
+     * @param $itemId
+     * @param null $itemType
+     * @return array|mixed
+     */
+    public function deleteProfile($itemId, $itemType = null)
+    {
+        return $this->_delete("profile/delete", [
+            "itemid" => $itemId,
+            "itemtype" => $itemType,
+        ]);
+    }
+
+    /**
+     * This method deletes a specific field of the profile which belongs to the item defined by the tenantid, itemid and the itemtypeid.
+     * The field can be addressed by a JSONPath expression.
+     *
+     * @param $itemId
+     * @param null $itemType
+     * @param $path
+     * @return array|mixed
+     */
+    public function deleteProfileField($itemId, $itemType = null, $path)
+    {
+        return $this->_delete("profile/field/delete", [
+            "itemid" => $itemId,
+            "itemtype" => $itemType,
+            "path" => $path
+        ]);
+    }
+
+
+    /**
+     * This method returns the profile of the item defined by the tenantid, itemid and the itemtypeid.
+     *
+     * @param $itemId
+     * @param $itemType
+     * @return array|mixed
+     */
+    public function load($itemId, $itemType)
+    {
+        return $this->_get("profile/load", [
+            "itemid" => $itemId,
+            "itemtype" => $itemType,
+        ]);
+    }
+
+    /**
+     * This method returns the profile of the item defined by the tenantid, itemid and the itemtypeid.
+     *
+     * @param $itemId
+     * @param $itemType
+     * @param $path
+     * @return array|mixed
+     */
+    public function loadProfileField($itemId, $itemType = null, $path)
+    {
+        return $this->_get("profile/field/load", [
+            "itemid" => $itemId,
+            "itemtype" => $itemType,
+            "path" => $path
+        ]);
+    }
+
+    /**
+     * This method stores a value into a specific field of the profile which belongs to the item defined by the tenantid, itemid and the itemtypeid.
+     * The field can be addressed by a JSONPath expression.
+     *
+     * @param $itemId
+     * @param $itemType
+     * @param $path
+     * @param $key
+     * @param $value
+     * @return array|mixed
+     */
+    public function storeProfileField($itemId, $itemType = null, $path, $key, $value)
+    {
+        return $this->_put("profile/field/store", [
+            "itemid" => $itemId,
+            "itemtype" => $itemType,
+            "path" => $path,
+            "key" => $key,
+            "value" => $value,
+        ]);
+    }
+
+    /**
+     * This method adds a value to a specific ARRAY field of the profile which belongs to the item defined by the tenantid, itemid and the itemtypeid.
+     * The field can be addressed by a JSONPath expression.
+     *
+     * @param $itemId
+     * @param $itemType
+     * @param $path
+     * @param $value
+     * @return array|mixed
+     */
+    public function pushProfileField($itemId, $itemType = null, $path, $value)
+    {
+        return $this->_put("profile/field/push", [
+            "itemid" => $itemId,
+            "itemtype" => $itemType,
+            "path" => $path,
+            "value" => $value,
+        ]);
+    }
+
+
+
 
 }
--- src/Api/Rankings.php
+++ src/Api/Rankings.php
@@ -20,7 +20,131 @@
 namespace Hafael\Easyrec\Api;
 
 
-class Rankings
+class Rankings extends Api
 {
+    /**
+     * This community ranking shows items that were viewed most by all users. 50 items are returned at most, results are sorted by relevance.
+     *
+     * @param null $clusterId
+     * @param null $requestedItemType
+     * @param null $timeRange
+     * @return array|mixed
+     */
+    public function mostViewedItems($clusterId = null, $requestedItemType = null, $withProfile = null, $timeRange = null, $startDate = null, $endDate = null)
+    {
+        return $this->_get("mostvieweditems", [
+            "clusterid" => $clusterId,
+            "requesteditemtype" => $requestedItemType,
+            "withProfile" => $withProfile,
+            "timeRange" => $timeRange,
+            "startDate" => $startDate,
+            "endDate" => $endDate,
+            "numberOfResults" => $this->getPerPage(),
+            "offset" => $this->getOffset(),
+        ]);
+    }
+
+    /**
+     * This community ranking shows items that were bought the most. No more than 50 items are returned, results are sorted by relevance.
+     *
+     * @param null $clusterId
+     * @param null $requestedItemType
+     * @param null $timeRange
+     * @return array|mixed
+     */
+    public function mostBoughtItems($clusterId = null, $requestedItemType = null, $withProfile = null, $timeRange = null, $startDate = null, $endDate = null)
+    {
+        return $this->_get("mostboughtitems", [
+            "clusterid" => $clusterId,
+            "requesteditemtype" => $requestedItemType,
+            "withProfile" => $withProfile,
+            "timeRange" => $timeRange,
+            "startDate" => $startDate,
+            "endDate" => $endDate,
+            "numberOfResults" => $this->getPerPage(),
+            "offset" => $this->getOffset(),
+        ]);
+    }
+
+    /**
+     * This community ranking shows items that were rated the most. No more than 50 items are returned, results are sorted by relevance.
+     *
+     * @param null $clusterId
+     * @param null $requestedItemType
+     * @param null $timeRange
+     * @return array|mixed
+     */
+    public function mostRatedItems($clusterId = null, $requestedItemType = null, $withProfile = null, $timeRange = null, $startDate = null, $endDate = null)
+    {
+        return $this->_get("mostrateditems", [
+            "clusterid" => $clusterId,
+            "requesteditemtype" => $requestedItemType,
+            "withProfile" => $withProfile,
+            "timeRange" => $timeRange,
+            "startDate" => $startDate,
+            "endDate" => $endDate,
+            "numberOfResults" => $this->getPerPage(),
+            "offset" => $this->getOffset(),
+        ]);
+    }
+
+    /**
+     * This community ranking shows the best rated items. Rating values range from 1 to 10.
+     * The ranking only includes items that have an average ranking value greater than 5.5. No more than 50 items are returned, results are sorted by relevance.
+     *
+     * @param null $clusterId
+     * @param null $userId
+     * @param null $requestedItemType
+     * @param null $withProfile
+     * @param null $timeRange
+     * @param null $startDate
+     * @param null $endDate
+     * @return array|mixed
+     */
+    public function bestRatedItems($clusterId = null, $userId = null, $requestedItemType = null, $withProfile = null, $timeRange = null, $startDate = null, $endDate = null)
+    {
+        return $this->_get("bestrateditems", [
+            "clusterid" => $clusterId,
+            "userid" => $userId,
+            "requesteditemtype" => $requestedItemType,
+            "withProfile" => $withProfile,
+            "timeRange" => $timeRange,
+            "startDate" => $startDate,
+            "endDate" => $endDate,
+            "numberOfResults" => $this->getPerPage(),
+            "offset" => $this->getOffset(),
+        ]);
+    }
+
+    /**
+     * This community ranking shows the worst rated items. Rating values range from 1 to 10.
+     * The ranking only includes items that have an average ranking value less than 5.5. No more than 50 items are returned, results are sorted by relevance.
+     *
+     * @param null $clusterId
+     * @param null $userId
+     * @param null $requestedItemType
+     * @param null $withProfile
+     * @param null $timeRange
+     * @param null $startDate
+     * @param null $endDate
+     * @return array|mixed
+     */
+    public function worstRatedItems($clusterId = null, $userId = null, $requestedItemType = null, $withProfile = null, $timeRange = null, $startDate = null, $endDate = null)
+    {
+        return $this->_get("worstrateditems", [
+            "clusterid" => $clusterId,
+            "userid" => $userId,
+            "requesteditemtype" => $requestedItemType,
+            "withProfile" => $withProfile,
+            "timeRange" => $timeRange,
+            "startDate" => $startDate,
+            "endDate" => $endDate,
+            "numberOfResults" => $this->getPerPage(),
+            "offset" => $this->getOffset(),
+        ]);
+    }
+
+
+
 
 }
