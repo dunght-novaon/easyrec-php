95003cc7da888f2138c5e6907f69067ff08de8b6
Dzung Ha The <dunght@novaon.asia>
2018-10-30 14:44:50 +0700

    Down version "guzzlehttp/guzzle" to ">=4.2,<6.0", edit .gitignore, init first testcase

---
+++ tests/EasyrecTest.php
@@ -0,0 +1,11 @@
+<?php
+
+class EasyrecTest extends \PHPUnit\Framework\TestCase
+{
+    public function testAdd()
+    {
+        $result = 1 + 1;
+        $this->assertEquals(2, $result, 'Wrong add 1 vs 1');
+        $this->assertEquals(3, $result, 'Wrong add 1 vs 1');
+    }
+}
